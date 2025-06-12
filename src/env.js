a9e701e92c8320c34e1b0c064cb5e644b4b450a5
Kaushik Mahadevan <kaushik.mahadevan@gmail.com>
2025-06-12 00:44:10 -0400

    addded some linting and shadcn button

---
+++ src/env.js
@@ -0,0 +1,14 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+    CLERK_SECRET_KEY: z.string(),
+  },
+  client: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+  },
+});
